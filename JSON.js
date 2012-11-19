36bd0d603accd2a46de9a9105a885db17fb563a2
Daniel Reyes <Dreyes007@fullsail.edu>
2012-11-19 09:32:24 -0500

    Added all necesary new files

---
+++ JSON.js
@@ -0,0 +1,54 @@
+var json = {
+	"Order1": {
+		"fname": ["First Name:", "King"],
+		"lname": ["Last name:", "Kong"],
+		"email": ["Email:", "Kkong@lives.com"],
+		"phone": ["Phone:", "123 456-7890"],
+		"address": ["Address:", "1234 Jungle Ave"],
+		"order": ["Order:", "Delivery"],
+		"payment": ["Payment:", "Visa"],
+		"select": ["Menus:", "Appetizer"],
+		"amount": ["Order Amount", "2"],
+		"date": ["Delivery date:", "2012-10-01"],
+		"comments": ["Additional Instructions:", "It's Alive!:"]
+	},
+	"Order2": {
+		"fname": ["First Name:", "Dan"],
+		"lname": ["Last name:", "King"],
+		"email": ["Email:", "Dking@lives.com"],
+		"phone": ["Phone:", "123 456-7890"],
+		"address": ["Address:", "1234 Amazon Ave"],
+		"order": ["Order:", "Carry-Out"],
+		"payment": ["Payment:", "Master Card"],
+		"select": ["Menus:", "Entree"],
+		"amount": ["Order Amount", "3"],
+		"date": ["Delivery date:", "2012-10-01"],
+		"comments": ["Additional Instructions:", "Yum Yum!:"]
+	},
+	"Order3": {
+		"fname": ["First Name:", "Marco"],
+		"lname": ["Last name:", "Polo"],
+		"email": ["Email:", "Marco@polo.com"],
+		"phone": ["Phone:", "123 456-7890"],
+		"address": ["Address:", "1234 Pool Ave"],
+		"order": ["Order:", "Delivery"],
+		"payment": ["Payment:", "Cash"],
+		"select": ["Menus:", "Dessert"],
+		"amount": ["Order Amount", "4"],
+		"date": ["Delivery date:", "2012-10-01"],
+		"comments": ["Additional Instructions:", "Hiyas!:"]
+	},
+	"Order4": {
+		"fname": ["First Name:", "Pedro"],
+		"lname": ["Last name:", "Jalapeno"],
+		"email": ["Email:", "Inastick@gmail.com"],
+		"phone": ["Phone:", "123 456-1234"],
+		"address": ["Address:", "9999 Comedy Ave"],
+		"order": ["Order:", "Delivery"],
+		"payment": ["Payment:", "Visa"],
+		"select": ["Menus:", "Appetizer"],
+		"amount": ["Order Amount", "1"],
+		"date": ["Delivery date:", "2012-11-22"],
+		"comments": ["Additional Instructions:", "Hi!:"]
+	}
+}
