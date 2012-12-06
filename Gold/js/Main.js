8e620d9bbad8ab21261bd3edc2a212a096c7eb3b
Daniel Reyes <Dreyes007@fullsail.edu>
2012-12-06 04:58:22 -0500

    Added validating functions to form.

--- Gold/js/Main.js
+++ Gold/js/Main.js
@@ -1,44 +1,34 @@
-// New main
+	// New main
 
+var parseplaceorder = function(data){
+	//uses form data here;
+	console.log(data);
+	
+};
 
 $(document).on('pageinit', function(){
-	//code needed for home page goes here
-});	
-		
-$('#addItem').on('pageinit', function(){
-
-		var orderForm = $('#placeorder');
-		    myForm.validate({
-			invalidHandler: function(form, validator) {
-			},
-			submitHandler: function() {
-		var data = myForm.serializeArray();
-			storeData(data);
+	
+	var oform = $('#placeorder')
+		odererrorlink = $('odererrorlink')
+	;
+	
+	oform.validate({
+		invalidHandler: function(form, validator){
+			odererrorlink.click();
+			var html = '';
+			for(var key in validator.submitted){
+				var label = $('label[for^="'+ key +'"]').not('[generated]');
+				var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+				var fieldName = legend.length ? legend.text() : label.text();
+				html += '<li>'+ fieldName +'</li>';
+			};
+			$("#ordererror ul").html(html);			
+		},
+		submitHandler: function(){
+			var data = oform.serializeArray();
+			parseplaceorder(data);
 		}
 	});
 	
-	//any other code needed for addItem page goes here
-	
 });
 
-//The functions below can go inside or outside the pageinit function for the page in which it is needed.
-
-//var autofillData = function (){
-	 
-//};
-
-//var getData = function(){
-
-//};
-
-//var storeData = function(data){
-	
-//}; 
-
-//var	deleteItem = function (){
-			
-//};
-					
-//var clearLocal = function(){
-
-//};
